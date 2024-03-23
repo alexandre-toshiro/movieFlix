8071a1f9ed97f67eef810b3efa8421b41b32ee09
Toshiro <toshiro.lyn@gmail.com>
2024-03-23 12:44:20 -0300

    GenreControllerIT - OK

---
+++ src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class GenreService {
+
+    private final GenreRepository repository;
+
+    public GenreService(GenreRepository repository) {
+        this.repository = repository;
+
+    }
+
+
+    public List<GenreDTO> findAll() {
+        List<Genre> entities = repository.findAll();
+        return entities.stream().map(GenreDTO::new).collect(Collectors.toList());
+    }
+}
